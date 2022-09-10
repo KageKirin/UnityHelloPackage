38ba38066374e255ac21ec0ac6fc43bf5f763675
Chris Helmich <kagekirin@gmail.com>
2022-09-10 20:25:22 +0200

    file: add HelloWindow editor pane

---
+++ Scripts/Editor/HelloWindow.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UIElements;
+
+namespace Hello
+{
+    public class HelloWindow : EditorWindow
+    {
+        [MenuItem("Hello/Window")]
+        public static void ShowWindow()
+        {
+            EditorWindow.GetWindow(typeof(HelloWindow));
+        }
+
+        void OnGUI()
+        {
+            EditorGUILayout.LabelField("Hello");
+        }
+    }
+}
